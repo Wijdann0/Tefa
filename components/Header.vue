e73a7de311bdea102ce6c95f2477a2ed8392818b
Wijdann0 <160085539+Wijdann0@users.noreply.github.com>
2024-02-29 09:40:30 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row pt-2   text-black text-center justify-content-center hede">
+    <div class="col-4 d-flex">
+      <div class="col ">
+        <img src="~/assets/img/logo.png" width="80" alt="LOGO-SMK4" class="">
+      </div>
+      <div class="col text-black tex">
+        <h1><u>PerpusDigital</u></h1>
+        <address>
+          SMKN 4 Tasikmalaya
+        </address>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<style >
+.logo {
+  width: 80%;
+}
+
+.hede {
+  background-color: #4D6EE3;
+}
+
+h1 {
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+</style>
