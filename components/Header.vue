193b1ca75dd2c7ff84d7e62bf4f82d8d1a575a9e
Wijdann0 <160085539+Wijdann0@users.noreply.github.com>
2024-03-01 05:57:50 +0700

    update font

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row pt-2 pb-2 bg-primary text-black d-flex justify-content-center">
+    <div class="col-4 d-flex justify-content-end">
+      <img src="~/assets/img/logo.png" alt="logo" width="80" height="90">
+    </div>
+    <div class="col-6">
+      <h1><u>PerpusDigital</u></h1>
+      <address>
+        SMKN 4 Tasikmalaya
+      </address>
+    </div>
+  </div>
+</template>
+
+<style >
+.logo {
+  width: 80%;
+}
+
+.hede {
+  background-color: #4D6EE3;
+}
+
+</style>
